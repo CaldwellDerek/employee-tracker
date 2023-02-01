ce204a2827347bf1861b5f600872ebc6f027c70b
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-01 09:58:44 -0800

    corrected foreign key restraints for role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -16,15 +16,17 @@ CREATE TABLE
         title VARCHAR(30),
         salary DECIMAL,
         department_id INT,
-        FOREIGN KEY (department_id) REFERENCES department(id)
+        CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE
+        SET NULL
     );
 
 CREATE TABLE
     employee (
         id INT AUTO_INCREMENT PRIMARY KEY,
         first_name VARCHAR(30),
-        last_name VARCHAR(30) role_id INT,
+        last_name VARCHAR(30),
         role_id INT,
         manager_id INT,
-        FOREIGN KEY (role_id) REFERENCES role(id)
+        CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE
+        SET NULL
     );
