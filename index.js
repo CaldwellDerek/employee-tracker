3b0b8bbe4cd6e97d121ba6751953bc77225e4e8d
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-31 22:48:03 -0800

    inital prompt created, db directory formed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// TODO
+const i = require("inquirer");
+
+const initialPrompt = async ()=> {
+    const menu = await i.prompt([
+        {
+            type: "list",
+            name: "menuChoice",
+            message: "Select from the following:",
+            choices: [
+                "- View All Departments", 
+                "- View All Roles", 
+                "- View All Employees", 
+                "- Add a Department", 
+                "- Add a Role", 
+                "- Add An Employee", 
+                "- Update an Employee Role"
+            ]
+
+        }
+    ])
+}
+
+initialPrompt();
